9a3682dd3970b4eea22d0d874215560abbbba7e0
Soumya-12025 <soumyaranjandas5498@gmail.com>
2023-05-02 20:15:21 +0530

    initial commit

---
+++ src/main/java/com/nt/SiluApplication.java
@@ -0,0 +1,13 @@
+package com.nt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiluApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiluApplication.class, args);
+	}
+
+}
